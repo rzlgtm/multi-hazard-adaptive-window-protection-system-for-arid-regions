0c65715190c9428fb845b8553dd022c5149ce435
raikhan <zrb0310@mail.ru>
2026-07-24 14:49:17 +0500

    Setting up a temperature and humidity sensor with an async 2-second delay so that future readings and motor control was not blocked by the time interval

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
+#include <DHT.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//temperature and humidity sensor init
+#define DHTPIN 1
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE); 
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+unsigned long lastDHTReadTime = 0;        //the last time DHT was read
+const unsigned long dhtInterval = 2000;   //breathing for DHT
+
+unsigned long currentTime;
+float humidity, temp;
+
+void setup(){
+  Serial.begin(9600);
+  dht.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop(){
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  currentTime = millis();
+  if (currentTime - lastDHTReadTime >= dhtInterval){
+    lastDHTReadTime = currentTime;
+    humidity = dht.readHumidity();
+    temp = dht.readTemperature();
+
+    //telemetry
+    if (isnan(humidity) || isnan(temp)) {
+      Serial.println(F("failed to read from h&t sensor"));
+    } else {
+      Serial.print(F("Humidity: ")); Serial.print(humidity);
+      Serial.print(F("%  Temperature: ")); Serial.print(temp); Serial.println(F("°C"));
+    }
+  }
+
 }
