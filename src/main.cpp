9716edfd5a0c4f2482099a459ab42daf67c2d59b
raikhan <zrb0310@mail.ru>
2026-07-26 13:17:02 +0500

    Added additional sensors with dummy values(for now.) Added variables that read the pins. Added additional telemetry reading loop, might merge the two loops together since the dht delay is shorter than the trened reading delay.

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <DHT.h>
 
 //temperature and humidity sensor init
-#define DHTPIN 1
-#define DHTTYPE DHT11
+const int DHTPIN = 1;
+const int DHTTYPE = DHT11;
 DHT dht(DHTPIN, DHTTYPE); 
 unsigned long lastDHTReadTime = 0;        //the last time DHT was read
 const unsigned long dhtInterval = 2000;   //breathing for DHT
@@ -17,11 +17,36 @@ const int freq = 30000;
 const int ledChannel = 0;
 const int resolution = 8;
 
+//MQ-135 gas detector
+const int gasRead = 4; //some analog pin
+int currentGas;
+
+//rain detector
+const int rainRead = 2; //some analog pin
+int currentRain;
+
+//photoresistor
+const int lightRead = 3; //some analog pin
+int initialLight;
+int currentLight;
+
+// trend, history tracking 
+unsigned long lastTrendCheckTime = 0;
+const unsigned long trendInterval = 5000; 
+int lightHistory[5] = {0};                 // sliding window for light fluctuations
+int lightIndex = 0;
+int baselineLight = 0;
+
+//states
+bool windowIsOpen = true;
+
 unsigned long currentTime;
 
 void closeWindow();
 void openWindow();
 void stopMotor();
+void sendPhoneNotification(String reason);
+
 
 void setup(){
   Serial.begin(9600);
@@ -33,6 +58,16 @@ void setup(){
 
   ledcSetup(ledChannel, freq, resolution);
   ledcAttachPin(motorEnA, ledChannel);
+
+  pinMode(gasRead, INPUT);
+  pinMode(rainRead, INPUT);
+  pinMode(lightRead, INPUT);
+
+  //TODO: light level baseline for swinging door trending algorithm
+  initialLight = analogRead(lightRead);
+  for(int i = 0; i < 5; i++) {
+    lightHistory[i] = initialLight;
+  }
 }
 
 void loop(){
@@ -50,6 +85,16 @@ void loop(){
       Serial.print(F("Humidity: ")); Serial.print(humidity);
       Serial.print(F("%  Temperature: ")); Serial.print(temp); Serial.println(F("°C"));
     }
+  }
+
+  if (currentTime-lastTrendCheckTime >= trendInterval){
+    lastTrendCheckTime = currentTime;
+
+    currentLight = analogRead(lightRead);
+    currentGas = analogRead(gasRead);
+    currentRain = analogRead(rainRead); // * maybe digitalRead??
+
+
   }
 
 }
